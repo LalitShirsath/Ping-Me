1cd20a5aec3f0155cecab63033c69631bcbc764d
Lalit Vilas Shirsath <lalit1112001@gmail.com>
2022-11-27 19:34:00 +0530

    Chat Dialog box added

--- client/src/components/Messenger.jsx
+++ client/src/components/Messenger.jsx
@@ -10,7 +10,14 @@ import LoginDialog from "./account/LoginDialog";
 import ChatDialog from './chat/ChatDialog';
 
 // this header will have properties of AppBar and our own styling
+
 const Header = styled(AppBar)`
+    height: 125px;
+    background-color: #00A884;
+    box-shadow:none;
+`
+
+const LoginHeader = styled(AppBar)`
     height: 220px;
     background-color: #00bfa5;
     box-shadow:none;
@@ -31,14 +38,22 @@ const Messenger = () => {
 
         <Component>
             {
-                account ? <ChatDialog/>
-                :
+                account ?
                 <>
                     <Header>
                         <Toolbar>
 
                         </Toolbar>
                     </Header>
+                    <ChatDialog/>
+                </>
+                :
+                <>
+                    <LoginHeader>
+                        <Toolbar>
+
+                        </Toolbar>
+                    </LoginHeader>
                     <LoginDialog/>
                 </>
             }
--- client/src/components/chat/ChatDialog.jsx
+++ client/src/components/chat/ChatDialog.jsx
@@ -1,8 +1,26 @@
+import { Dialog } from "@mui/material"
 
+const dialogStyle = {
+    height: "95%",
+    width: "100%",
+    margin: "20px",
+    maxWidth: "100%",
+    maxHeight: "100%",
+    borderRadius: 0,
+    boxShadow: "none",
+    overflow: "hidden"
+}
 
 const ChatDialog = () => {
+
     return (
-        <p>Hello from chat </p>
+        <Dialog
+            open={true}
+            PaperProps={{sx: dialogStyle}}
+            hideBackdrop = {true}
+        >
+
+        </Dialog>
     )
 }
 
