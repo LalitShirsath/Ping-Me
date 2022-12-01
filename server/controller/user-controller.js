c72eb75444bcda15698fcd77c023b9adcc8f389e
Lalit Vilas Shirsath <lalit1112001@gmail.com>
2022-12-01 23:50:43 +0530

    added router and controller

---
+++ server/controller/user-controller.js
@@ -0,0 +1,5 @@
+
+
+export const addUser = (req, res) => {
+    console.log(req.body);
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 // new syntax as per ES6
 import express  from "express";
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 // at server side writing extension is compulsory so add .js ahead of database/db 
 import Connection from "./database/db.js";
+import Route from "./routes/route.js";
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({express:true}));
+app.use(bodyParser.urlencoded({extended:true})); // when we search with some space then browser add random chars e.g. what is bodyParser => what%90is%90bodyParser
+app.use('/', Route); 
 
 Connection();
 
