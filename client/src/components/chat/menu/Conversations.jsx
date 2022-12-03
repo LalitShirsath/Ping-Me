c22f4dd34380d9bc025446477876cf3e11882557
Lalit Vilas Shirsath <lalit1112001@gmail.com>
2022-12-03 13:10:46 +0530

    profile photo loading problem solved

--- client/src/components/chat/menu/Conversations.jsx
+++ client/src/components/chat/menu/Conversations.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState , useContext} from "react";
 
-import { Box, Divider, styled } from "@mui/material";
+import { Box, Divider, Typography, styled } from "@mui/material";
 
 import { getUsers } from "../../../service/api";
 import { AccountContext } from "../../../context/AccountProvider";
 
 
 //components
-import Conversation from "./Conversation";
+// import Conversation from "./Conversation";
 
 const Component = styled(Box)`
     height: 81vh;
@@ -20,6 +20,21 @@ const StyledDivider = styled(Divider)`
     background-color: #e9edef;
     opacity: .6;
 `;
+
+const Compo = styled(Box)`
+    display: flex;
+    height: 45px;
+    padding: 13px 0;
+    cursor: pointer;
+`
+
+const Image = styled('img')({
+    width: 50,
+    height: 50,
+    borderRadius: '50%',
+    padding: '0 14px',
+    objectFit: 'cover'
+})
 
 const Conversations = () => {
 
@@ -42,7 +57,21 @@ const Conversations = () => {
                 users.map(user => (
                     user.sub !== account.sub &&
                     <>
-                        <Conversation user = {user}/>
+                        <Compo>
+
+                            <Box>
+                                <Image src={user.picture} alt="dp"/>
+                                
+                            </Box>
+
+                            <Box>
+                                <Box>
+                                    <Typography>{user.name}</Typography>
+                                </Box>
+                            </Box>
+
+                        </Compo>
+
                         <StyledDivider/>
                     </>
                 ))
